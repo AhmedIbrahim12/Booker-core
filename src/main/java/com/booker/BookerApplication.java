9a9d4e278e8fca269b4c5174e59a8330badbe3d8
AhmedIbrahim12 <ahmed.mohamed.ibrahim.12@gmail.com>
2019-03-23 15:09:12 +0200

    Adding file upload and download

--- src/main/java/com/booker/BookerApplication.java
+++ src/main/java/com/booker/BookerApplication.java
@@ -2,8 +2,12 @@ package com.booker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.booker.configurationproperties.FileStorageProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({ FileStorageProperties.class })
 public class BookerApplication {
 
 	public static void main(String[] args) {
